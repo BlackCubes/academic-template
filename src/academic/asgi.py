9775f4809e029de29e5630ef4f6bf365fe109976
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-11-13 13:20:16 -0800

    refactor(project): rename old project names to new one

--- src/academic/asgi.py
+++ src/academic/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for spedscore project.
+ASGI config for academic project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spedscore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academic.settings')
 
 application = get_asgi_application()
