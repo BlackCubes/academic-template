451fdaeab65a6d392a616b199b9b4e6633059259
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-11-21 11:20:04 -0800

    feat(grade-app): start new app

--- src/grade/apps.py
+++ src/grade/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GradeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'grade'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "grade"
